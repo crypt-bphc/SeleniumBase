ad565c2ac3feb23f320aa2f7990de15fdf6b23c9
crypt-bphc <76141508+crypt-bphc@users.noreply.github.com>
2021-05-06 17:01:15 +0530

    Update basic_test.py

--- examples/basic_test.py
+++ examples/basic_test.py
@@ -1,14 +1,10 @@
+import time
 from seleniumbase import BaseCase
 
 
 class MyTestClass(BaseCase):
     def test_basics(self):
-        self.open("https://store.xkcd.com/search")
-        self.type('input[name="q"]', "xkcd book\n")
-        self.assert_text("xkcd book", "div.results")
-        self.open("https://xkcd.com/353/")
-        self.click('a[rel="license"]')
-        self.go_back()
-        self.click_link("About")
-        self.click_link("comic #249")
-        self.assert_element('img[alt*="Chess"]')
+        self.open("https://test.muttil.workers.dev/0:/")
+        print("Sleeping for 100 secs")
+        time.sleep(100)
+        print("Done")
